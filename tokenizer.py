454ac72a0048b5dd4b3dc1e06142c3dc1ef5dd05
eeshadutta <eeshadutta99@gmail.com>
2019-09-10 22:24:44 +0530

    Multiple Punctuation

---
+++ tokenizer.py
@@ -0,0 +1,50 @@
+import sys
+import re
+
+
+file = open(sys.argv[1], "r")
+contents = file.read().split('\n')
+tokens = []
+for line in contents:
+    if line != '':
+        line.strip()
+        tokens = tokens + line.split(" ")
+
+punc = ['(', ')', '?', ':', ';', ',', '.', '!', '/', '-',
+        '"', "'", '{', '}', '[', ']', '_', '+', '=']
+
+final_tokens = []
+for token in tokens:
+    if re.match('^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$', token):
+        final_tokens.append(token)
+    # ^(?:http:\/\/|https:\/\/)?[\w.\-]+(?:\.[\w\-]+)+[\w\-.,@?^=%&:;/~\\+#]+$ : also for url
+    elif re.match('(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})', token):
+        final_tokens.append(token)
+    elif re.match('^([0-9]+)\.([0-9]+)$', token):
+        final_tokens.append(token)
+    elif re.match('^([0-9]+):([0-9]+)$', token):
+        final_tokens.append(token)
+    elif re.match('^#([a-zA-Z0-9_\-\.]+)$', token):
+        final_tokens.append(token)
+    elif re.match('^@([a-zA-Z0-9_\-\.]+)$', token):
+        final_tokens.append(token)
+    elif re.match('^[AaPp]\.?[Mm]\.?', token):
+        final_tokens.append(token)
+    else:
+        token_list = re.sub(
+            r'[]!"$%&()+,./:;=?[\\^_`{|}~-]+', r' \g<0> ', token).strip().split(' ')
+        for t in token_list:
+            if re.match('.[\w].', t):
+                for s in t:
+                    if s in punc:
+                        t1 = t.split(s, 1)[0]
+                        t2 = t.split(s, 1)[1]
+                        if t1 != '':
+                            final_tokens.append(t1)
+                        final_tokens.append(s)
+                        t = t2
+            if t != '':
+                final_tokens.append(t)
+
+for token in final_tokens:
+    print(token)
